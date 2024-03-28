c85474663cf6ebf3ecb4e915f2f7fc6ac92336a5
MarekUlatowski <marek.ulatowski@atos.net>
2024-03-28 12:10:53 +0100

    some comments

--- jobs/my_jobs.py
+++ jobs/my_jobs.py
@@ -1,11 +1,11 @@
 from nautobot.extras.jobs import Job, StringVar
 class SimpleLogJob(Job):
-class Meta:
-name = "Simple Log Message"
+    class Meta:
+        name = "Simple Log Message"
 description = "Logs a simple message to the job output."
 message = StringVar(
 description="This is me doing code "
 )
 def run(self,**kwargs):
-self.logger.info(str(kwargs))
-return kwargs
+    self.logger.info(str(kwargs))
+    return kwargs
